9022cd406cfbe607c0b9bdfffffd1f657499e4c1
dylan <monetizedesign@gmail.com>
2021-06-15 15:04:37 -0700

    design changez

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -1,10 +1,12 @@
 import { create } from '@storybook/theming'
 
 export default create({
-	base: 'dark',
+	base: 'light',
 	brandTitle: 'Appkit.io UI',
 	brandUrl: 'https://appkit.io',
 	brandImage:
-		'https://p195.p4.n0.cdn.getcloudapp.com/items/NQuoK7PO/912dcf13-e5cb-4375-8a56-ea0224427b91.png?v=eb0994693a0f6b2bde00ab6508228383',
-	appContentBg: '#FFF'
+		'https://p195.p4.n0.cdn.getcloudapp.com/items/v1uY4LAY/82cbcdf9-b50a-451c-9fad-1a62f3147873.png?source=viewer&v=d07ef028deb5ea414dcb2a64dc052c6c',
+	appBg: '#f6f6f7',
+	colorPrimary: '#42474b',
+	colorSecondary: '#058f6c'
 })
