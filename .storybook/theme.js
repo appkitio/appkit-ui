e3c1eafcaa1e9e7c63bf3d21bed598883b3810d5
dylan <monetizedesign@gmail.com>
2021-06-15 15:00:31 -0700

    build storybook bg color change

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -5,5 +5,6 @@ export default create({
 	brandTitle: 'Appkit.io UI',
 	brandUrl: 'https://appkit.io',
 	brandImage:
-		'https://p195.p4.n0.cdn.getcloudapp.com/items/NQuoK7PO/912dcf13-e5cb-4375-8a56-ea0224427b91.png?v=eb0994693a0f6b2bde00ab6508228383'
+		'https://p195.p4.n0.cdn.getcloudapp.com/items/NQuoK7PO/912dcf13-e5cb-4375-8a56-ea0224427b91.png?v=eb0994693a0f6b2bde00ab6508228383',
+	appContentBg: '#FFF'
 })
